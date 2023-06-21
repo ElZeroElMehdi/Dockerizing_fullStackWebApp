f64797c4a995f1d4e530dfed977bf510486fa453
El mehdi Elmohammady <eelmoham@e1r2p15.1337.ma>
2023-06-21 22:58:35 +0100

    done

--- server/index.js
+++ server/index.js
@@ -1,60 +1,3 @@
-// const keys = require('./keys');
-
-// //export Application setup
-// const express = require('express')
-// const bodyparser = require('body-parser')
-// const cors = require('cors')
-
-// const app = express()
-// // app.use(cors())
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-// }))
-// app.use(bodyparser.json());
-
-// const { Pool } = require('pg')
-
-// const pgClient = new Pool({
-//     user: keys.pgUser,
-//     host: keys.pgHost,
-//     database: keys.pgDatabase,
-//     password: keys.pgPassword,
-//     port: keys.pgPort
-// });
-
-
-// pgClient.on("connect", client => {
-//     client
-//         .query("CREATE TABLE IF NOT EXISTS values (number INT)")
-//         .catch(err => console.log("PG ERROR", err));
-// });
-
-
-// //Express route definitions
-// app.get("/", (req, res) => {
-//     res.send("Hi");
-// });
-
-// // get the values
-// app.get("/values/all", async (req, res) => {
-//     const values = await pgClient.query("SELECT * FROM values");
-
-//     res.send(values);
-// });
-
-
-// // now the post -> insert value
-// app.post("/values", async (req, res) => {
-//     if (!req.body.value) res.send({ working: false });
-
-//     pgClient.query("INSERT INTO values(number) VALUES($1)", [req.body.value]);
-
-//     res.send({ working: true });
-// });
-
-// app.listen(5000, err => {
-//     console.log("Listening");
-// });
 const keys = require("./keys");
 
 // Express Application setup
@@ -109,6 +52,68 @@ app.post("/values", async (req, res) => {
     }
 
 });
+
+
+
+
+
+
+
+
+
+// Define API endpoints
+// Define API endpoints
+app.post('/signup', async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+  
+      // Check if the username or email already exists in the database
+      const existingUser = await pgClient.query(
+        'SELECT * FROM users WHERE username = $1 OR email = $2',
+        [username, email]
+      );
+  
+      if (existingUser.rows.length > 0) {
+        return res.status(400).json({ error: 'Username or email already exists' });
+      }
+  
+      // Insert the new user into the database
+      const newUser = await pgClient.query(
+        'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *',
+        [username, email, password]
+      );
+  
+      res.json({ message: 'Signup successful', user: newUser.rows[0] });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
+    }
+  });
+  
+
+
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Check if the username and password match the stored data in the database
+        const user = await pool.query(
+            'SELECT * FROM users WHERE username = $1 AND password = $2',
+            [username, password]
+        );
+
+        if (user.rows.length === 0) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+
+        res.json({ message: 'Login successful', user: user.rows[0] });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+
 
 app.listen(5000, err => {
     console.log("Listening");
